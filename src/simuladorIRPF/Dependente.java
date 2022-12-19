d84d0bb921aa96134d56f10b0ffe018b0673ca2c
Ailton Aires Amado <ailtonaires5@gmail.com>
2022-12-19 15:56:11 -0300

    implementation of the dependent registration class

--- src/simuladorIRPF/Dependente.java
+++ src/simuladorIRPF/Dependente.java
@@ -1,5 +1,13 @@
 package simuladorIRPF;
 
 public class Dependente {
+	private String nomeDependente;
+	private String dataNascimento;
+	
+	public Dependente(String nomeDependente, String dataNascimento) {
+		super();
+		this.nomeDependente = nomeDependente;
+		this.dataNascimento = dataNascimento;
+	}
 
 }
