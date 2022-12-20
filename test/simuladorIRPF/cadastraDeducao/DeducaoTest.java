d486de9ea59750f10003a37617183b7b0c0b02b4
geraldovictor <geraldovictor@outlook.com>
2022-12-20 19:26:21 -0300

    Corrige erro de compilação

--- test/simuladorIRPF/cadastraDeducao/DeducaoTest.java
+++ test/simuladorIRPF/cadastraDeducao/DeducaoTest.java
@@ -16,19 +16,19 @@ public class DeducaoTest {
         simul = new SimuladorIRPF();
     }
     
-//	@Test
-//    public void testeCadastraDeducaoUm() {
-//        simul.cadastrarDeducao("Previdencia privada", 1500f);
-//        
-//        assertEquals(1500f, simul.getDeducao(),0f);
-//    }
-//
-//	@Test
-//    public void testeCadastraDeducaoDois() {
-//        simul.cadastrarDeducao("Previdencia privada", 1200f);
-//        
-//        assertEquals(1200f, simul.getDeducao(),0f);
-//    }
+	@Test
+    public void testeCadastraDeducaoUm() throws Exception {
+        simul.cadastrarDeducao("Previdencia privada", 1500f);
+        
+        assertEquals(1500f, simul.getDeducao(),0f);
+    }
+
+	@Test
+    public void testeCadastraDeducaoDois() throws Exception	{
+        simul.cadastrarDeducao("Previdencia privada", 1200f);
+        
+        assertEquals(1200f, simul.getDeducao(),0f);
+    }
 	
 	@Test
     public void testeCadastraDuasDeducao() throws Exception {
