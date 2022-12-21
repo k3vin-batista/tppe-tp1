51c456561ff77d2d17e0bce8dfe09f857e85839a
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2022-12-20 21:36:12 -0300

    Teste Exceptions - Pensão Alimentícia

--- test/simuladorIRPF/cadastraPensao/PensaoAlimenticiaTest.java
+++ test/simuladorIRPF/cadastraPensao/PensaoAlimenticiaTest.java
@@ -1,6 +1,10 @@
 package simuladorIRPF.cadastraPensao;
 
 import static org.junit.Assert.assertEquals;
+
+import Exceptions.DescricaoEmBrancoException;
+import Exceptions.ValorDeducaoInvalidoException;
+
 import org.junit.Before;
 import org.junit.Test;
 import simuladorIRPF.SimuladorIRPF;
@@ -32,4 +36,16 @@ class PensaoAlimenticiaTest {
 		simuladorIRPF.cadastrarPensaoAlimenticia("Segunda Pensão", 250f);
 		assertEquals(730f, simuladorIRPF.getPensaoAlimenticia(),0f);
 	}
+	
+	@Test(expected = DescricaoEmBrancoException.class)
+ 	public void testeDescEmBranco() throws Exception {
+		simuladorIRPF.cadastrarPensaoAlimenticia(" ", 1000f);
+
+ 	}
+
+ 	@Test(expected = ValorDeducaoInvalidoException.class)
+ 	public void testeValorMenorOuIgualZero() throws Exception {
+ 		simuladorIRPF.cadastrarPensaoAlimenticia(" Carnê", 0f);
+
+ 	}
 }
