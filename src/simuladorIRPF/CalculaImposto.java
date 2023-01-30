cb3403ea224d2efb991b9090216d30f51462fa1f
João Victor <joao15victor08@gmail.com>
2023-01-30 16:14:01 -0300

    Refactoring: Extrair constantes Mau-cheiro identificado: Utilização de números magicos, variável temporária para armazenar resultado de expressão e modificação de variável temporária fora de um laço de repetição. Descrição da refatoração: Extrair números magicos em constantes, substituir variável temporária por uma consulta e utilização de variáveis para cada situação. Classes/métodos/atributos afetados: * Classe CalculaImposto -> números magicos substituidos por constantes. * Método calcularImposto -> utilização de consultas no lugar de variáveis temporárias. * Atributo base -> foi dividido em 5 atributos, um para cada faixa.
    Co-authored-by: Luis Furtado <luisaraujo@arcoeducacao.com.br>
    Co-authored-by: Geraldo Victor <geraldovictor@outlook.com>

--- src/simuladorIRPF/CalculaImposto.java
+++ src/simuladorIRPF/CalculaImposto.java
@@ -2,42 +2,49 @@ package simuladorIRPF;
 
 public class CalculaImposto {
 	private SimuladorIRPF simulador;
-	private Imposto impostoTemp;
-	private float baseCalculo;
+	//private Imposto impostoTemp;
+	//private float baseCalculo;
+	private static final float LIMITE_1_FAIXA = 1903.98f;
+	private static final float LIMITE_2_FAIXA = 922.67f;
+	private static final float LIMITE_3_FAIXA = 924.40f;
+	private static final float LIMITE_4_FAIXA = 913.63f;
+	private static final float ALIQUOTA_1_FAIXA = 7.5f/100;
+	private static final float ALIQUOTA_2_FAIXA = 15f/100;
+	private static final float ALIQUOTA_3_FAIXA = 22.5f/100;
+	private static final float ALIQUOTA_4_FAIXA = 27.5f/100;
+	private float baseCalculo1Faixa;
+	private float baseCalculo2Faixa;
+	private float baseCalculo3Faixa;
+	private float baseCalculo4Faixa;
+	private float baseCalculo5Faixa;
+	
 
 	public CalculaImposto(SimuladorIRPF simuladorFonte) {
 		this.simulador = simuladorFonte;
+		this.baseCalculo1Faixa = this.simulador.getBaseCalculo();
+		this.baseCalculo2Faixa = baseCalculo1Faixa - LIMITE_1_FAIXA;
+		this.baseCalculo3Faixa = baseCalculo2Faixa - LIMITE_2_FAIXA;
+		this.baseCalculo4Faixa = baseCalculo3Faixa - LIMITE_3_FAIXA;
+		this.baseCalculo5Faixa = baseCalculo4Faixa - LIMITE_4_FAIXA;
 	}
+
 
 	public void calcularImposto() {
-		this.baseCalculo = this.simulador.getBaseCalculo();
-		this.calcularFaixa(this.baseCalculo);
-	}
-
-	public void calcularFaixa(Float base) {
-		if (base > 1903.98f) {
+		if (this.baseCalculo1Faixa > LIMITE_1_FAIXA) {
 			// segunda faixa
-			base -= 1903.98f;
-			this.impostoTemp = new Imposto((Math.min(base, 922.67f) * 7.5f / 100), 2);
-			this.simulador.adicionaImposto(this.impostoTemp);
-			if (base > 922.67f) {
-
+			this.simulador.adicionaImposto(new Imposto((Math.min(this.baseCalculo2Faixa, LIMITE_2_FAIXA) * ALIQUOTA_1_FAIXA), 2));
+			
+			if (this.baseCalculo2Faixa > LIMITE_2_FAIXA) {
 				// terceira faixa
-				base -= 922.67f;
-				this.impostoTemp = new Imposto((Math.min(base, 924.40f) * 15f / 100), 3);
-				this.simulador.adicionaImposto(this.impostoTemp);
-				if (base > 924.40f) {
-
+				this.simulador.adicionaImposto(new Imposto((Math.min(this.baseCalculo3Faixa, LIMITE_3_FAIXA) * ALIQUOTA_2_FAIXA), 3));
+				
+				if (this.baseCalculo3Faixa > LIMITE_3_FAIXA) {
 					// quarta faixa
-					base -= 924.40f;
-					this.impostoTemp = new Imposto((Math.min(base, 913.63f) * 22.5f / 100), 4);
-					this.simulador.adicionaImposto(this.impostoTemp);
-					if (base > 913.63f) {
-
+					this.simulador.adicionaImposto(new Imposto((Math.min(this.baseCalculo4Faixa, LIMITE_4_FAIXA) * ALIQUOTA_3_FAIXA), 4));
+					
+					if (this.baseCalculo4Faixa > 913.63f) {
 						// quinta faixa
-						base -= 913.63f;
-						this.impostoTemp = new Imposto((base * 27.5f / 100), 5);
-						this.simulador.adicionaImposto(this.impostoTemp);
+						this.simulador.adicionaImposto(new Imposto((this.baseCalculo5Faixa * ALIQUOTA_4_FAIXA), 5));
 
 					}
 				}
