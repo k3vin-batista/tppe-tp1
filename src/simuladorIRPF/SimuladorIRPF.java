08306b9f20b945634858f56570d6096483a7649c
Kevin Luis <k.luis360@gmail.com>
2023-01-30 09:47:22 -0300

    Refactoring: Extrair Metodo
    Mau(s) cheiro(s) identificado(s):  Método inchado, condicional extensa
    Descrição da refatoração: Foi aplicada a refactoração para dividir o
    método original em 2 métodos, melhorando assim a coesão de cada um.
    
    Classes/métodos/atributos afetados: Método calculaImposto() da Classe
    SimuladorIRPF foi dividido nos métodos calculaImposto() e
    calculaFaixa().
    
    Co-authored-by: Ailton Aires <ailtonaires5@gmail.com>
    Co-authored-by: Guilherme Fernandes <guilhermedfs11@gmail.com>

--- src/simuladorIRPF/SimuladorIRPF.java
+++ src/simuladorIRPF/SimuladorIRPF.java
@@ -25,87 +25,83 @@ public class SimuladorIRPF {
 
 	private static List<Rendimento> rendimentos;
 	private float totalRendimento;;
-	
+
 	private float totalImposto;
 	private float baseCalculo;
 	private static List<Imposto> impostos;
-	
+
 	private float aliquota;
 
-	
 	public SimuladorIRPF() {
 		rendimentos = new ArrayList<Rendimento>();
 		this.totalRendimento = 0;
-		
+
 		dependente = new LinkedList<Dependente>();
-		
+
 		pensoesAlimenticias = new LinkedList<PensaoAlimenticia>();
-		
+
 		previdenciasOficiais = new LinkedList<PrevidenciaOficial>();
-		
+
 		this.totalImposto = 0;
 		this.baseCalculo = 0;
 		impostos = new ArrayList<Imposto>();
-		
+
 		this.aliquota = 0;
 	}
-	
-	
 
 	public void cadastrarRendimento(String descricao, float valor) {
-		
-		
+
 		Rendimento temp = new Rendimento(descricao, valor);
-		
+
 		Boolean adicionado = rendimentos.add(temp);
-		
-		if(adicionado) {
+
+		if (adicionado) {
 			this.totalRendimento += valor;
 		}
-	
+
 	}
-	
+
 	public float getTotalRendimento() {
 		return totalRendimento;
 	}
-	
-	public float cadastrarPrevidenciaOficial(String descricao, float valorPrevidencia) throws DescricaoEmBrancoException, ValorDeducaoInvalidoException {
-		
+
+	public float cadastrarPrevidenciaOficial(String descricao, float valorPrevidencia)
+			throws DescricaoEmBrancoException, ValorDeducaoInvalidoException {
+
 		// -=-=-=-=-=-=- Validations -=-=-=-=-=-=-
 
 		if (descricao.trim().length() < 1) {
 			throw new DescricaoEmBrancoException();
 		}
-		
+
 		if (valorPrevidencia <= 0) {
 			throw new ValorDeducaoInvalidoException();
 		}
-		
+
 		// -=-=-=-=-=-=- Business Rules -=-=-=-=-=-=-
-		
+
 		PrevidenciaOficial novaPrevidenciaOficial = new PrevidenciaOficial(descricao, valorPrevidencia);
 
 		this.previdenciasOficiais.add(novaPrevidenciaOficial);
-		
+
 		this.valorTotalPrevidenciasOficiaisTriangulacao += novaPrevidenciaOficial.valor;
-		
+
 		return novaPrevidenciaOficial.valor;
 	}
 
-	
 	public float getValorTotalPrevidenciasOficiais() {
 		return this.valorTotalPrevidenciasOficiaisTriangulacao; // Teste de triangulação
 	}
-	
+
 	public void cadastroDependente(String nomeDependente, String dataDeNascimento) throws Exception {
-		if(nomeDependente.trim().length()<1) {
+		if (nomeDependente.trim().length() < 1) {
 			throw new NameIsNullTrowException();
 		}
 		Dependente dependente = new Dependente(nomeDependente, dataDeNascimento);
 		this.dependente.add(dependente);
 		this.valorTotalDependenteTriangulacao += valorDependente;
 	}
-	
+
 	public void setarBaseCalculo(float baseCalculo) {
 		this.baseCalculo = baseCalculo;
 	}
@@ -113,99 +109,100 @@ public class SimuladorIRPF {
 	public float getBaseCalculo() {
 		return this.baseCalculo;
 	}
-	
+
 	public void calculaImposto() {
-		
-		Imposto imp;
+
 		float base = this.baseCalculo;
+		this.calculaFaixa(base);
 
-		if (base > 1903.98f){
-            
+	}
+
+	public void calculaFaixa(float base) {
+		Imposto imposto;
+		if (base > 1903.98f) {
 			// segunda faixa
-            base -= 1903.98f;
-            imp = new Imposto((Math.min(base, 922.67f) * 7.5f/100), 2); 
-            impostos.add(imp);
-            if(base > 922.67f) {
-                
-            	// terceira faixa
-                base -= 922.67f;
-                imp = new Imposto((Math.min(base, 924.40f) * 15f/100), 3); 
-                impostos.add(imp);
-                if (base > 924.40f) {
-                    
-                	// quarta faixa
-                    base -= 924.40f;
-                    imp = new Imposto((Math.min(base, 913.63f) * 22.5f/100), 4);
-                    impostos.add(imp);
-                    if(base > 913.63f) {
-                        
-                    	// quinta faixa
-                        base -= 913.63f;
-                        imp = new Imposto(( base * 27.5f/100), 5);
-                        impostos.add(imp);
+			base -= 1903.98f;
+			imposto = new Imposto((Math.min(base, 922.67f) * 7.5f / 100), 2);
+			impostos.add(imposto);
+			if (base > 922.67f) {
 
-                    }
-                }
-            }    
-        }
+				// terceira faixa
+				base -= 922.67f;
+				imposto = new Imposto((Math.min(base, 924.40f) * 15f / 100), 3);
+				impostos.add(imposto);
+				if (base > 924.40f) {
+
+					// quarta faixa
+					base -= 924.40f;
+					imposto = new Imposto((Math.min(base, 913.63f) * 22.5f / 100), 4);
+					impostos.add(imposto);
+					if (base > 913.63f) {
+
+						// quinta faixa
+						base -= 913.63f;
+						imposto = new Imposto((base * 27.5f / 100), 5);
+						impostos.add(imposto);
+
+					}
+				}
+			}
+		}
+
 	}
 
 	public float getTotalImposto() {
-		for(Imposto i : impostos){
+		for (Imposto i : impostos) {
 			this.totalImposto += i.getValor();
 		}
 		return this.totalImposto;
-	}	
-	
+	}
+
 	public float getDependente() {
 		return this.valorTotalDependenteTriangulacao; // Teste de Triangulacao
 	}
 
-	public void cadastrarDeducao(String descricaoDeducao, float valorDeducao) throws Exception  {
-		if(descricaoDeducao.trim().length()<1) {
+	public void cadastrarDeducao(String descricaoDeducao, float valorDeducao) throws Exception {
+		if (descricaoDeducao.trim().length() < 1) {
 			throw new DescricaoEmBrancoException();
 		}
-		if(valorDeducao<=0) {
+		if (valorDeducao <= 0) {
 			throw new ValorDeducaoInvalidoException();
 		}
-		
-		Deducao deducao = new Deducao(descricaoDeducao,valorDeducao);
-		this.valorTotalDeducao += valorDeducao;	
+
+		Deducao deducao = new Deducao(descricaoDeducao, valorDeducao);
+		this.valorTotalDeducao += valorDeducao;
 	}
 
 	public float getDeducao() {
-		return this.valorTotalDeducao ; //Duplicacao
+		return this.valorTotalDeducao; // Duplicacao
 	}
-	
-	public void cadastrarPensaoAlimenticia(
-			String descricaoPensaoAlimenticia, 
-			float valorPensaoAlimenticia) throws Exception
-	{
-		if(descricaoPensaoAlimenticia.trim().length() < 1) {
+
+	public void cadastrarPensaoAlimenticia(String descricaoPensaoAlimenticia, float valorPensaoAlimenticia)
+			throws Exception {
+		if (descricaoPensaoAlimenticia.trim().length() < 1) {
 			throw new DescricaoEmBrancoException();
 		}
-		
-		if(valorPensaoAlimenticia <= 0) {
+
+		if (valorPensaoAlimenticia <= 0) {
 			throw new ValorDeducaoInvalidoException();
 		}
-		
+
 		PensaoAlimenticia novaPensao = new PensaoAlimenticia(descricaoPensaoAlimenticia, valorPensaoAlimenticia);
 		this.pensoesAlimenticias.add(novaPensao);
-		
+
 		this.valorTotalPensaoAlimenticia += valorPensaoAlimenticia;
 	}
-	
+
 	public float getPensaoAlimenticia() {
 		return this.valorTotalPensaoAlimenticia; // Triangulação
 	}
-	
 
 	public float getAliquota() {
 		return this.aliquota;
 	}
 
 	public void setarAliquota() {
-		this.aliquota = ( this.getTotalImposto()/this.getTotalRendimento() ) * 100;
+		this.aliquota = (this.getTotalImposto() / this.getTotalRendimento()) * 100;
 	}
-	
+
 }
